fca8e5e25787dfa9f08f459d288159f71d80ffbb
Ausin Formica <austin.formica@gmail.com>
2015-11-17 13:29:13 -0500

    boil angular app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + '/public'))
    .use(cookieParser());
 
 app.get('/', function(req, res) {
